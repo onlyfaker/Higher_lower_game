e0f2f5d70556e2a4c2af55d188aa4c1d3d286722
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-03-21 17:28:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,34 @@ import random
 from art import LOGO,VS
 from word_dictionary import INSTAGRAM_USERS_DICTIONARY
 
+def return_proffesion(user):
+    return insta_users[user]["profession"]
+def return_country(user):
+    return insta_users[user]["country"]
+def return_followers(user):
+    return insta_users[user]["followers"]
+
+def more_followers(user_a,user_b):
+    if return_followers(user_a) > return_followers(user_b):
+        return True
+    else:
+        return False
+def random_user_choice(instagram_users_dict):
+    return random.choice(list(instagram_users_dict.keys()))
+
+
 print(LOGO)
 insta_users = INSTAGRAM_USERS_DICTIONARY
 
-random_choice = random.choice(list(insta_users.keys()))
 
-print(random_choice)
-print(insta_users[random_choice]["followers"])
+A_user = random_user_choice(insta_users)
+B_user = random_user_choice(insta_users)
+
+print(A_user,',',return_proffesion(A_user),',',return_country(A_user),return_followers(A_user))
+print(VS)
+print(B_user,',',return_proffesion(B_user),',',return_country(B_user),return_followers(B_user))
+
+print(more_followers(A_user,B_user))
+
+
+
