4221011c04070ef0fe65a25bf11cc986ea62d0bf
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-03-21 16:15:00 -0400

    todo's

---
+++ main.py
@@ -0,0 +1,4 @@
+#todo - generate a random key from a list of instagram users
+#todo - compare random 2 ones and see which one has more followers,
+#todo - take the B answer and make it an A anser if you are correct, and generate a random one for another B answer
+#todo - continue until you are wrong and show final score
