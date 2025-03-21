61504b059aa17f7f38c6762fce4a3d81c924661c
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-03-21 16:27:29 -0400

    Create word_dictionary.py

---
+++ word_dictionary.py
@@ -0,0 +1,152 @@
+INSTAGRAM_USERS_DICTIONARY = {
+    "Maluma": {
+        "profession": "Musician",
+        "country": "Colombia",
+        "followers": 64000000
+    },
+    "Cristiano Ronaldo": {
+        "profession": "Footballer",
+        "country": "Portugal",
+        "followers": 650000000
+    },
+    "Kylie Jenner": {
+        "profession": "Entrepreneur",
+        "country": "USA",
+        "followers": 394000000
+    },
+    "Zendaya": {
+        "profession": "Actress",
+        "country": "USA",
+        "followers": 179000000
+    },
+    "Dua Lipa": {
+        "profession": "Musician",
+        "country": "UK",
+        "followers": 89000000
+    },
+    "Nike": {
+        "profession": "Sportswear Brand",
+        "country": "USA",
+        "followers": 301000000
+    },
+    "National Geographic": {
+        "profession": "Media Company",
+        "country": "USA",
+        "followers": 279000000
+    },
+    "Instagram": {
+        "profession": "Social Media Platform",
+        "country": "Global",
+        "followers": 650000000
+    },
+    "NASA": {
+        "profession": "Space Agency",
+        "country": "USA",
+        "followers": 90000000
+    },
+    "Beyoncé": {
+        "profession": "Musician",
+        "country": "USA",
+        "followers": 312000000
+    },
+    "Justin Bieber": {
+        "profession": "Musician",
+        "country": "Canada",
+        "followers": 294000000
+    },
+    "Lionel Messi": {
+        "profession": "Footballer",
+        "country": "Argentina",
+        "followers": 505000000
+    },
+    "Kim Kardashian": {
+        "profession": "Entrepreneur",
+        "country": "USA",
+        "followers": 357000000
+    },
+    "Virat Kohli": {
+        "profession": "Cricketer",
+        "country": "India",
+        "followers": 270000000
+    },
+    "Apple": {
+        "profession": "Tech Company",
+        "country": "USA",
+        "followers": 300000000
+    },
+    "Louis Vuitton": {
+        "profession": "Luxury Fashion Brand",
+        "country": "France",
+        "followers": 55000000
+    },
+    "Shakira": {
+        "profession": "Musician",
+        "country": "Colombia",
+        "followers": 90000000
+    },
+    "Netflix": {
+        "profession": "Streaming Service",
+        "country": "USA",
+        "followers": 45000000
+    },
+    "FC Barcelona": {
+        "profession": "Football Club",
+        "country": "Spain",
+        "followers": 173000000
+    },
+    "Real Madrid": {
+        "profession": "Football Club",
+        "country": "Spain",
+        "followers": 173000000
+    },
+    "Adidas": {
+        "profession": "Sportswear Brand",
+        "country": "Germany",
+        "followers": 30000000
+    },
+    "Selena Gomez": {
+        "profession": "Musician",
+        "country": "USA",
+        "followers": 421000000
+    },
+    "Kanye West": {
+        "profession": "Musician",
+        "country": "USA",
+        "followers": 18000000
+    },
+    "BMW": {
+        "profession": "Automobile Manufacturer",
+        "country": "Germany",
+        "followers": 36000000
+    },
+    "Tesla": {
+        "profession": "Automobile Manufacturer",
+        "country": "USA",
+        "followers": 10000000
+    },
+    "Google": {
+        "profession": "Tech Company",
+        "country": "USA",
+        "followers": 26000000
+    },
+    "Amazon": {
+        "profession": "E-commerce",
+        "country": "USA",
+        "followers": 42000000
+    },
+    "Disney": {
+        "profession": "Entertainment Company",
+        "country": "USA",
+        "followers": 40000000
+    },
+    "McDonald's": {
+        "profession": "Fast Food Chain",
+        "country": "USA",
+        "followers": 52000000
+    },
+    "The Rock": {
+        "profession": "Actor",
+        "country": "USA",
+        "followers": 394000000
+    }
+}
