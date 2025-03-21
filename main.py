f6ded4ce22a7c744a274b9d4b1a2b4bc5f6c9b35
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-03-21 16:47:11 -0400

    random functionality import

--- main.py
+++ main.py
@@ -2,3 +2,15 @@
 #todo - compare random 2 ones and see which one has more followers,
 #todo - take the B answer and make it an A anser if you are correct, and generate a random one for another B answer
 #todo - continue until you are wrong and show final score
+import random
+
+from art import LOGO,VS
+from word_dictionary import INSTAGRAM_USERS_DICTIONARY
+
+print(LOGO)
+insta_users = INSTAGRAM_USERS_DICTIONARY
+random_choice = random.choice(list(insta_users.keys()))
+print(random_choice)
+
+print(insta_users[random_choice]["followers"])
+# for item in insta_users:
